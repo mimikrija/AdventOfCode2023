eb69785c750046f62b44ecdb1d0b6c54eb7e62e6
Maja Gacesa <maja.gacesa@gmail.com>
2023-12-01 10:00:58 +0100

    [day01] somewhat cleaned solution

--- python/01.py
+++ python/01.py
@@ -1,39 +1,31 @@
 from santas_little_helpers.helpers import *
-from re import findall
+import re
 
 input_data = read_input(1)
-party_1 = 0
-for line in input_data:
-    numbers = findall(r'\d', line)
-    party_1 += int(numbers[0]+numbers[-1])
-
-
-
+party_1 = sum(int((digits:=re.findall(r'\d', line))[0]+digits[-1]) for line in input_data)
 
 party_2 = 0
 
-words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-words_reversed = [word[::-1] for word in words]
+words = ['\d', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+words_reversed = ['\d']+[word[::-1] for word in words[1:]]
 
-bla = '|'.join(c for c in words)
-truc = '|'.join(c for c in words_reversed)
+forward = re.compile('|'.join(c for c in words))
+backward = re.compile('|'.join(c for c in words_reversed))
 
-def convert(s):
-    if len(s) == 1:
-        return int(s)
-    return words.index(s)+1
+first_digit = lambda x: re.search(forward, x)[0]
+last_digit = lambda x: re.search(backward, x[::-1])[0]
 
-def converts(s):
-    if len(s) == 1:
-        return int(s)
-    return words_reversed.index(s)+1
+def convert_to_numbers(text, search_words):
+    if len(text) == 1:
+        return text
+    return str(search_words.index(text))
 
+def digits_to_number(d1, d2):
+    d1 = convert_to_numbers(d1, words)
+    d2 = convert_to_numbers(d2, words_reversed)
+    return int(d1+d2)
 
-for line in input_data:
-    numbers = findall(f'\d|{bla}', line)
-    fun = findall(f'\d|{truc}', line[::-1])
-    print(line, numbers[0], numbers[-1], convert(numbers[0]), convert(numbers[-1]), int(str(convert(numbers[0]))+str(convert(numbers[-1]))))
-    party_2 += int(str(convert(numbers[0]))+str(converts(fun[0])))
+party_2 = sum(digits_to_number(first_digit(line), last_digit(line)) for line in input_data)
 
 print_solutions(party_1, party_2)
 
@@ -43,3 +35,4 @@ def test_one():
 
 def test_two():
     assert party_2 == 53312
+

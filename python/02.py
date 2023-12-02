da3e916993d5af8dcec06593ed26eb58d31d1a97
Maja Gacesa <maja.gacesa@gmail.com>
2023-12-02 20:03:42 +0100

    [day02] probably unreadable but I can't help comprehensions

--- python/02.py
+++ python/02.py
@@ -1,36 +1,21 @@
 from santas_little_helpers.helpers import *
 import re
+from math import prod
 
 
 input_data = read_input(2)
 
-print(input_data[:4])
+dice_data = [(n, re.compile(rf'(\d+) ({color})')) for n, color in enumerate(('red', 'green', 'blue'), start=12)]
 
-cubes_in_bag = {"red": 12, "green": 13, "blue": 14}
 
-find_red = re.compile(r'(\d+) (red)')
-find_green = re.compile(r'(\d+) (green)')
-find_blue = re.compile(r'(\d+) (blue)')
+party_1 = sum(n for n, line in enumerate(input_data, start=1)
+                  if all(not any(int(c[0]) > max_dice for c in re.findall(pattern, line))
+                         for max_dice, pattern in dice_data))
 
-party_1 = 0
-party_2 = 0
-for ID, line in enumerate(input_data, start=1):
-    c = re.findall(find_red, line)
-    possible_red = not any(int(rc[0]) > cubes_in_bag["red"] for rc in c)
-    min_red = max(int(rc[0]) for rc in c)
+party_2 = sum(prod(max(int(c[0]) for c in re.findall(pattern, line))
+                   for _, pattern in dice_data) 
+                   for line in input_data)
 
-    c = re.findall(find_green, line)
-    possible_green = not any(int(rc[0]) > cubes_in_bag["green"] for rc in c)
-    min_green = max(int(rc[0]) for rc in c)
-    
-    c = re.findall(find_blue, line)
-    possible_blue = not any(int(rc[0]) > cubes_in_bag["blue"] for rc in c)
-    min_blue= max(int(rc[0]) for rc in c)
-
-    party_2 += min_blue * min_red * min_green
-
-    if possible_red and possible_green and possible_blue:
-        party_1 += ID
 
 print_solutions(party_1, party_2)
 

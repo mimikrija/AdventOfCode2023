31333b19d67b4448c3cd5d7ebb08c16b93411105
Maja Gacesa <maja.gacesa@gmail.com>
2023-12-03 20:22:23 +0100

    [day03] cleanup

--- python/03.py
+++ python/03.py
@@ -1,5 +1,6 @@
 from santas_little_helpers.helpers import *
 import re
+from math import prod
 
 
 def get_neighbors(x, y):
@@ -24,23 +25,17 @@ for y, row in enumerate(input_data):
         motor_data.append((int(num), {(p, y) for p in positions}))
 
 
-party_1 = 0
-for num, coords in motor_data:
-    neighbours = {n for (x, y) in coords for n in get_neighbors(x, y) }
-    if any(neighbor in symbols for neighbor in neighbours):
-        party_1 += num
-party_2 = 0
+party_1 = sum(num for num, coords in motor_data 
+              if any(neighbor in symbols 
+                     for neighbor in {n for (x, y) in coords for n in get_neighbors(x, y)}))
 
+
+party_2 = 0
 for gx, gy in gears:
     gear_neighbours = get_neighbors(gx, gy)
-    count = 0
-    res = 1
-    for num, coords in motor_data:
-        if any (c in gear_neighbours for c in coords):
-            res*=num
-            count += 1
-    if count == 2:
-        party_2 +=res
+    adj = [num for num, coords in motor_data if any(c in gear_neighbours for c in coords)]
+    if len(adj) == 2:
+        party_2 += prod(adj)
 
 
 print_solutions(party_1, party_2)

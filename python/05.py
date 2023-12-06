bb7e0d4ac116600f2fb524ce43ecef3233a94e02
Maja Gacesa <maja.gacesa@gmail.com>
2023-12-06 10:46:37 +0100

    [day05] ranges for pt2, very tedious and slow

--- python/05.py
+++ python/05.py
@@ -1,10 +1,10 @@
 from santas_little_helpers.helpers import *
 import re
+from math import inf
 
 numbers = re.compile(r'\d+')
 
 input_data = read_input(5, separator='\n\n')
-input_data = get_input('inputs/05e.txt', separator='\n\n')
 
 seeds = list(map(int, re.findall(numbers, input_data[0])))
 
@@ -77,6 +77,8 @@ def generate_mapping_ranges(mapping):
     mapping = sorted(mapping, key=lambda x: x[1])
     holes = []
     sorted_ranges = []
+    hole_left = mapping[0][1]
+    hole_right = mapping[-1][1]+mapping[-1][-1]
     # then check if there are any holes:
     for (dl, _, l), (Dl, _, _) in zip(mapping, mapping[1:]):
         dest_right = dl + l
@@ -87,22 +89,43 @@ def generate_mapping_ranges(mapping):
     for dl, sl, l in mapping:
         sorted_ranges.append(((sl, sl+l), (dl, dl+l)))
     
+    if hole_left != 0:
+        holes.append((0, hole_left))
+    holes.append((hole_right, inf))
     return (sorted_ranges, holes)
 
+
 sorted_ranges = [generate_mapping_ranges(mapping) for mapping in mappings]
+
 # source, destination, holes
 
 def generate_whatever(our_range, sorted_range):
-    source_ranges, destination_ranges = sorted_range[0]
+    source_destination_ranges = sorted_range[0]
     holes = sorted_range[1]
     final_destination_ranges = []
-    for source, dest in zip(source_ranges, destination_ranges):
-        final_destination_ranges.append(reduce_dest(source, dest, intersect(our_range, source)))
+    for source, dest in source_destination_ranges:
+        intersection = intersect(our_range, source)
+        if intersection:
+            final_destination_ranges.append(reduce_dest(source, dest, intersection))
     for h in holes:
-        final_destination_ranges.append(reduce_dest(source, source, intersect(our_range, h)))
-
+        intersection = intersect(our_range, h)
+        if intersection:
+            final_destination_ranges.append(reduce_dest(source, source, intersection))
     return final_destination_ranges
 
 
+
 for s in seed_ranges:
-    print(generate_whatever(s, sorted_ranges[0]))
+    new_seeds = [s]
+    for sorted_range in sorted_ranges:
+        new_seeds = [c for new_seed in new_seeds for c in generate_whatever(new_seed, sorted_range)]
+    party_2 = min(c[0] for c in new_seeds)
+
+
+print_solutions(party_1, party_2)
+
+def test_one():
+    assert party_1 == 313045984
+
+def test_two():
+    assert party_2 == 20283860

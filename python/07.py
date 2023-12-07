77b6ae43dd1a2e8cbc667d567a288bded5457945
Maja Gacesa <maja.gacesa@gmail.com>
2023-12-07 14:34:16 +0100

    [day07] trick to provide optional arg to sorting lambda!

--- python/07.py
+++ python/07.py
@@ -11,7 +11,7 @@ def card_rank(card, part_2=False):
         if len(groups) <= 2:
             return 'five'
         if len(groups) == 4:
-                return 'three of a kind'
+                return 'three'
         if count['J'] >= 3:
                 return 'four'
         elif len(groups) == 3:
@@ -29,45 +29,38 @@ def card_rank(card, part_2=False):
     if groups[0][0] == 2 and groups[1][0] == 2:
         return 'two pairs'
     if len(groups) == 3 and groups[0][0] == 3:
-        return 'three of a kind'
+        return 'three'
     if groups[0][0] == 2:
         return 'pair'
     return 'high card'
 
-
-def compare(card1, card2, rank_1, rank_2, card_labels):
-    RANKINGS = ['high card', 'pair', 'two pairs', 'three of a kind', 'full house', 'four', 'five']
-    if RANKINGS.index(rank_1) > RANKINGS.index(rank_2):
-        return 1
-    elif RANKINGS.index(rank_1) < RANKINGS.index(rank_2):
-        return -1
-    else: # same ranking
-        for l, r in zip(card1, card2):
-            if l != r:
-                return 1 if card_labels.index(l) > card_labels.index(r) else -1
-
-def compare_cards(card1, card2):
-    rank_1 = card_rank(card1)
-    rank_2 = card_rank(card2)
-    return compare(card1, card2, rank_1, rank_2, card_labels='23456789TJQKA')
-
-def compare_cards_joker(card1, card2):
-    rank_1 = card_rank(card1, part_2=True)
-    rank_2 = card_rank(card2, part_2=True)
-    return compare(card1, card2, rank_1, rank_2, card_labels='J23456789TQKA')
-
-
+def compare_helper(part_2=False):
+    def compare(card1, card2):
+        rank_1 = card_rank(card1, part_2)
+        rank_2 = card_rank(card2, part_2)
+        RANKINGS = ['high card', 'pair', 'two pairs', 'three', 'full house', 'four', 'five']
+        card_labels='J23456789TQKA' if part_2 else '23456789TJQKA'
+        if RANKINGS.index(rank_1) > RANKINGS.index(rank_2):
+            return 1
+        elif RANKINGS.index(rank_1) < RANKINGS.index(rank_2):
+            return -1
+        else: # same ranking, compare by first card
+            for l, r in zip(card1, card2):
+                if l != r:
+                    return 1 if card_labels.index(l) > card_labels.index(r) else -1
+    return compare
 
 
 input_data = read_input(7)
 cards = {(sp:=line.split())[0]: int(sp[1]) for line in input_data}
 
+def total_winnings(cards, part_2=False):
+    rankings = sorted(cards, key=cmp_to_key(compare_helper(part_2)))
+    return sum(rank*cards[card] for rank, card in enumerate(rankings, start=1))
 
-rankings = sorted(cards, key=cmp_to_key(compare_cards))
-party_1 = sum(rank*cards[sorted_card] for rank, sorted_card in enumerate(rankings, start=1))
 
-rankings = sorted(cards, key=cmp_to_key(compare_cards_joker))
-party_2 = sum(rank*cards[sorted_card] for rank, sorted_card in enumerate(rankings, start=1))
+party_1, party_2 = (total_winnings(cards, part_2) for part_2 in (False, True))
+
 
 print_solutions(party_1, party_2)
 

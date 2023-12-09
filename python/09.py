1113a199ed1c0b485b566cf064a447a4b482b76c
Maja Gacesa <maja.gacesa@gmail.com>
2023-12-09 19:42:55 +0100

    [day09] part two is just reverse

--- python/09.py
+++ python/09.py
@@ -4,21 +4,15 @@ from santas_little_helpers.helpers import *
 input_data = read_input(9)
 
 
-sign = lambda x: 1 if x%2 == 0 else -1
-def mixsum(seq):
-    return sum(s*sign(n) for n, s in enumerate(seq))
-
 def solve(sequence, part_2=False):
+    if part_2:
+        sequence.reverse()
     last = sequence[-1]
-    firsts = [sequence[0]]
     while True:
         sequence = [right-left for left, right in zip(sequence[:-1], sequence[1:])]
         if all(c == 0 for c in sequence):
-            if part_2:
-                return mixsum(firsts)
             return last
         last += sequence[-1]
-        firsts.append(sequence[0])
 
 
 sequences = [list(map(int, line.split())) for line in input_data]

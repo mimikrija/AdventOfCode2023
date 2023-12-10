3060859cdc40a4d36339b53139fb89d09bc74782
Maja Gacesa <maja.gacesa@gmail.com>
2023-12-10 07:00:21 +0100

    just divide came from by two because the loop guarantees that all of the came froms are on the path

---
+++ python/10.py
@@ -0,0 +1,79 @@
+from santas_little_helpers.helpers import *
+from collections import deque
+
+
+input_data = read_input(10)
+input_data = get_input('inputs/09e.txt')
+
+NORTH = 0-1j
+SOUTH = 0+1j
+EAST = 1+0j
+WEST = -1+0j
+
+DIRECTIONS = {
+    '-': (EAST, WEST), # east- west
+    '|': (NORTH, SOUTH),  # north-south
+    'L': (NORTH, EAST),
+    'J': (NORTH, WEST),
+    '7': (SOUTH, WEST),
+    'F': (SOUTH, EAST),
+
+}
+
+pipes = {}
+
+for y, line in enumerate(input_data):
+    for x, c in enumerate(line):
+        if c != '.' and c != 'S':
+            pipes[complex(x, y)] = DIRECTIONS[c]
+        elif c == 'S':
+            START = complex(x, y)
+            pipes[START] = (WEST, SOUTH)
+
+
+MATCHES = {
+    NORTH: SOUTH,
+    SOUTH: NORTH,
+    EAST: WEST,
+    WEST: EAST,
+}
+
+
+
+def get_neighbors(location, heightmap):
+    return {candidate for delta in pipes[location]
+            if (candidate:=location + delta) in heightmap}
+
+
+def get_path_length(came_from, start, end):
+    current = end
+    path = []
+    while current != start:
+        if current not in came_from:
+            return
+        path.append(current)
+        current = came_from[current]
+    return len(path)
+
+
+def shortest_path(heightmap, start=None):
+    frontier = deque([start])
+    came_from = {start: None}
+    while frontier:
+        current = frontier.popleft()
+
+        if current == start and len(came_from) >1 : # full loop
+                print(came_from)
+                print('hello')
+                return get_path_length(came_from, start, start)
+
+        for neighbor in get_neighbors(current, heightmap):
+            if neighbor not in came_from:
+                frontier.append(neighbor)
+                came_from[neighbor] = current
+            # if neighbor == START:
+            #     frontier.append(neighbor)
+            #     came_from[neighbor] = current
+    print(len(came_from)//2)
+
+print(shortest_path(pipes, start = START))

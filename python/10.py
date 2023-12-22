493bba298debf800c4f50903852ee3070a12ebc9
Maja Gacesa <maja.gacesa@gmail.com>
2023-12-22 14:57:39 +0100

    [day10] clean up redundant stuff

--- python/10.py
+++ python/10.py
@@ -12,13 +12,12 @@ EAST = 1+0j
 WEST = -1+0j
 
 DIRECTIONS = {
-    '-': (EAST, WEST), # east- west
-    '|': (NORTH, SOUTH),  # north-south
+    '-': (EAST, WEST),
+    '|': (NORTH, SOUTH),
     'L': (NORTH, EAST),
     'J': (NORTH, WEST),
     '7': (SOUTH, WEST),
     'F': (SOUTH, EAST),
-
 }
 
 pipes = {}
@@ -29,7 +28,8 @@ for y, line in enumerate(input_data):
             pipes[complex(x, y)] = DIRECTIONS[c]
         elif c == 'S':
             START = complex(x, y)
-            pipes[START] = (WEST, SOUTH)
+            pipes[START] = (WEST, SOUTH) # this is manual depending on the example
+
 
 
 MATCHES = {
@@ -41,81 +41,60 @@ MATCHES = {
 
 
 
-def get_neighbors(location, heightmap):
+def get_neighbors(location, pipemap):
     return {candidate for delta in pipes[location]
-            if (candidate:=location + delta) in heightmap}
+            if (candidate:=location + delta) in pipemap}
 
 
-def get_path_length(came_from, start, end):
-    current = end
-    path = []
-    while current != start:
-        if current not in came_from:
-            return
-        path.append(current)
-        current = came_from[current]
-    return len(path)
-
-def is_angle(coord):
-    angles = 'LJF7'
-    return any(pipes[coord] == DIRECTIONS[angle] for angle in angles)
-
-def shortest_path(heightmap, start=None):
+def get_loop(pipemap, start=None):
     frontier = deque([start])
-    came_from = {start: None}
+    visited = {start}
     while frontier:
         current = frontier.popleft()
-
-        if current == start and len(came_from) >1 : # full loop
-                return get_path_length(came_from, start, start)
-
-        for neighbor in get_neighbors(current, heightmap):
-            if neighbor not in came_from:
+        for neighbor in get_neighbors(current, pipemap):
+            if neighbor not in visited:
                 frontier.append(neighbor)
-                came_from[neighbor] = current
-            # if neighbor == START:
-            #     frontier.append(neighbor)
-            #     came_from[neighbor] = current
-    print(len(came_from)//2) #party_1
-    return set(came_from)
+                visited.add(neighbor)
+    return visited
 
 
-#print(shortest_path(pipes, start = START))
+loop = get_loop(pipes, start = START)
+party_1 = len(loop)//2
 
-loop = shortest_path(pipes, start = START)
-
-clean_map = [[c for c in line] for line in input_data]
+# pt2:
+party_2 = 0
 for y, line in enumerate(input_data):
+    cline = ''
     for x, c in enumerate(line):
         if complex(x, y) not in loop:
-            clean_map[y][x] = '.'
-        if c =='S':
-            clean_map[y][x] = '7' # this is manual depending on the example
-party_2 = 0
-for n, line in enumerate(clean_map):
-    
-    cline = ''.join(c for c in line)
+            cline += '.'
+        elif c =='S':
+        # this is manual depending on the example
+            cline += '7'
+        else:
+            cline += c
+
     matches = re.finditer(r'L-*J|L-*7|F-*J|F-*7|\|', cline)
-    areas = re.finditer(r'\.+', cline)
-    spans_to_check = []
-    for a in areas:
-        spans_to_check.append(a.span())
-
-
-    out = True
-    last_right = -1
+    is_in = False
+    previous_right = -1
     for match in matches:
-        conf = match.group()
+        pipe_conf = match.group()
         left, right = match.span()
-        diff = left - last_right - 1
-        
+        area_between_pipes = left - previous_right - 1
 
-        if not out:
-            #print(f'line number {n}, {left}, {last_right}')
-            party_2 += diff
-        last_right = right-1
-        if not(('L' in conf and 'J' in conf) or ('F' in conf and '7' in conf)):
-            out = not out
+        if is_in:
+            party_2 += area_between_pipes
+        previous_right = right-1
+        # LJ and F7 combos do not change the in/out status
+        # so switch it only if we hit the other kind (hence not)
+        is_in ^= not(any(all(c in pipe_conf for c in ins) for ins in ('LJ','F7')))
 
 
-print(party_2)
+
+print_solutions(party_1, party_2)
+
+def test_one():
+    assert party_1 == 6864
+
+def test_two():
+    assert party_2 == 349

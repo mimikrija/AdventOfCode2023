47af35426efdfbab274436a1a7af8a167828c3d8
Maja Gacesa <maja.gacesa@gmail.com>
2024-11-07 09:15:19 +0100

    [day13] part2: we can use same functions with full/smudged criteria

--- python/13.py
+++ python/13.py
@@ -5,8 +5,13 @@ input_data = read_input(13, separator='\n\n')
 
 images = [{(x, y) for y, row in enumerate(image.split('\n'), start=1) for x, symbol in enumerate(row, start=1) if symbol == '#'} for image in input_data]
 
+def is_full_mirror(full_image, cropped_image, mirrored_image):
+    return all( c in full_image for c in mirrored_image) and all(c in mirrored_image for c in cropped_image)
 
-def horizontal_mirror(image):
+def is_smudged_mirror(full_image, cropped_image, mirrored_image):
+    return {len(mirrored_image-cropped_image), len(cropped_image-mirrored_image)} == {0,1}
+
+def horizontal_mirror(image, criterion):
     MAX_X = max(x for x, y in image)
     for horizontal_axis in range(1, MAX_X):
         mirror = set()
@@ -18,11 +23,11 @@ def horizontal_mirror(image):
                     mirror.add((new_x, y))
         axis_distance = min(MAX_X-horizontal_axis, horizontal_axis)
         cropped = {(x, y) for x, y in image if x in range(horizontal_axis-axis_distance + 1, horizontal_axis + 1)}
-        if all( c in image for c in mirror) and all(c in mirror for c in cropped):
+        if criterion(image, cropped, mirror):
             return horizontal_axis
     return 0
 
-def vertical_mirror(image):
+def vertical_mirror(image, criterion):
     MAX_Y = max(y for x, y in image)
     for horizontal_axis in range(1, MAX_Y):
         mirror = set()
@@ -34,15 +39,20 @@ def vertical_mirror(image):
                     mirror.add((x, new_y))
         axis_distance = min(MAX_Y-horizontal_axis, horizontal_axis)
         cropped = {(x, y) for x, y in image if y in range(horizontal_axis-axis_distance + 1, horizontal_axis + 1)}
-        if all( c in image for c in mirror) and all(c in mirror for c in cropped):
+        if criterion(image, cropped, mirror):
             return horizontal_axis
     return 0
 
 
 
-party_1 = sum(horizontal_mirror(image) + 100*vertical_mirror(image) for image in images)
-print_solutions(party_1)
+party_1, party_2 = (sum(horizontal_mirror(image, criterion) + 100*vertical_mirror(image, criterion) for image in images) for criterion in (is_full_mirror, is_smudged_mirror))
+
+print_solutions(party_1,party_2)
+
 
 
 def test_one():
     assert party_1 == 40006
+
+def test_two():
+    assert party_2 == 28627

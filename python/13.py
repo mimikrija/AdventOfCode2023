80697799d3e372b2ea2282dbf9c460f36961d1fb
Maja Gacesa <maja.gacesa@gmail.com>
2024-11-07 10:28:26 +0100

    [day15] instead of two (practically duplicated) functions
    use one, but flip the coordinates instead

--- python/13.py
+++ python/13.py
@@ -4,6 +4,7 @@ from santas_little_helpers.helpers import *
 input_data = read_input(13, separator='\n\n')
 
 images = [{(x, y) for y, row in enumerate(image.split('\n'), start=1) for x, symbol in enumerate(row, start=1) if symbol == '#'} for image in input_data]
+flipped_images = [{(y, x) for x, y in image} for image in images]
 
 def is_full_mirror(full_image, cropped_image, mirrored_image):
     return all( c in full_image for c in mirrored_image) and all(c in mirrored_image for c in cropped_image)
@@ -11,41 +12,28 @@ def is_full_mirror(full_image, cropped_image, mirrored_image):
 def is_smudged_mirror(full_image, cropped_image, mirrored_image):
     return {len(mirrored_image-cropped_image), len(cropped_image-mirrored_image)} == {0,1}
 
-def horizontal_mirror(image, criterion):
-    MAX_X = max(x for x, y in image)
-    for horizontal_axis in range(1, MAX_X):
+
+def find_mirror_axis(image, criterion):
+    max_row_or_column = max(x for x, _ in image)
+    for axis in range(1, max_row_or_column):
         mirror = set()
         for x, y in image:
-            if x > horizontal_axis:
-                distance = x - horizontal_axis - 1
-                new_x = horizontal_axis - distance
-                if new_x in range(1, horizontal_axis + 1):
+            if x > axis:
+                distance = x - axis - 1
+                new_x = axis - distance
+                if new_x in range(1, axis + 1):
                     mirror.add((new_x, y))
-        axis_distance = min(MAX_X-horizontal_axis, horizontal_axis)
-        cropped = {(x, y) for x, y in image if x in range(horizontal_axis-axis_distance + 1, horizontal_axis + 1)}
+        axis_distance = min(max_row_or_column-axis, axis)
+        limit_range = range(axis-axis_distance + 1, axis + 1)
+        cropped = {(x, y) for x, y in image if x in limit_range}
         if criterion(image, cropped, mirror):
-            return horizontal_axis
-    return 0
-
-def vertical_mirror(image, criterion):
-    MAX_Y = max(y for x, y in image)
-    for horizontal_axis in range(1, MAX_Y):
-        mirror = set()
-        for x, y in image:
-            if y > horizontal_axis:
-                distance = y - horizontal_axis - 1
-                new_y = horizontal_axis - distance
-                if new_y in range(1, horizontal_axis + 1):
-                    mirror.add((x, new_y))
-        axis_distance = min(MAX_Y-horizontal_axis, horizontal_axis)
-        cropped = {(x, y) for x, y in image if y in range(horizontal_axis-axis_distance + 1, horizontal_axis + 1)}
-        if criterion(image, cropped, mirror):
-            return horizontal_axis
+            return axis
     return 0
 
 
-
-party_1, party_2 = (sum(horizontal_mirror(image, criterion) + 100*vertical_mirror(image, criterion) for image in images) for criterion in (is_full_mirror, is_smudged_mirror))
+party_1, party_2 = (sum(find_mirror_axis(image, criterion) + 100*find_mirror_axis(flipped_image, criterion)
+                        for image, flipped_image in zip(images, flipped_images))
+                            for criterion in (is_full_mirror, is_smudged_mirror))
 
 print_solutions(party_1,party_2)
 

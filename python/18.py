57c7833c2b59922c560828e1257a528eaf68e53f
Maja Gacesa <maja.gacesa@gmail.com>
2024-11-13 23:25:54 +0100

    [day18] shoelace formula

--- python/18.py
+++ python/18.py
@@ -5,53 +5,61 @@ from collections import defaultdict
 input_data = read_input(18)
 
 # parse instructions
-instructions = [((spl:=line.split())[0], int(spl[1]), spl[2]) for line in input_data]
+instructions_part_1 = [((spl:=line.split())[0], int(spl[1])) for line in input_data]
+instructions_part_2 = list()
+for line in input_data:
+    color = line.split()[2]
+    dir = int(color[-2])
+    count = int(color[2:7], 16)
+    instructions_part_2.append((dir, count))
+
 
 DIRECTIONS = {
-    'U': 0-1j,
-    'D': 0+1j,
-    'L':-1+0j,
     'R': 1+0j,
-    'UL': -1-1j,
-    'UR': 1-1j,
-    'DL': -1+1j,
-    'DR': 1+1j,
+    0:   1+0j,
+
+    'D': 0+1j,
+    1:   0+1j,
+
+    'L':-1+0j,
+    2:  -1+0j,
+
+    'U': 0-1j,
+    3:   0-1j,
 }
 
-def expand_inwards(edge, start):
-    frontier = {start}
-    inside = {start}
-    while frontier:
-        current = frontier.pop()
-        if current in outer_edge:
-            continue
-        for neighbor in (current + dir for dir in DIRECTIONS.values()):
-            if neighbor not in edge and neighbor not in inside:
-                inside.add(neighbor)
-                frontier.add(neighbor)
-    return len(inside) + len(outer_edge)
+def calculate_vertices(instructions):
+    current = 0+0j
+    vertices = [current]
+    for dir, count in instructions:
+        current += DIRECTIONS[dir] * count
+        vertices.append(current)
+    return vertices
 
 
-# draw outer edge
-outer_edge = set()
-pos = 0+0j
-for dir, count, color in instructions:
-    outer_edge |= {pos+n*DIRECTIONS[dir] for n in range(1, count+1)}
-    pos += count*DIRECTIONS[dir]
+def shoelace_formula(vertices):
+    list_vertices = vertices + [vertices[0]]
+    left_shoelace = sum(a.real * b.imag for a, b in zip(list_vertices, list_vertices[1:]))
+    right_shoelace = sum(a.imag * b.real for a, b in zip(list_vertices, list_vertices[1:]))
+    area = abs(left_shoelace - right_shoelace) //2
+    return int(area)
 
 
-rows = defaultdict(list)
-for c in outer_edge:
-    rows[int(c.imag)].append(int(c.real))
+def perimeter(instructions):
+    return sum(ins[1] for ins in instructions)
 
-rows = {row: sorted(columns) for row, columns in rows.items()}
+def calculate_area(instructions):
+    vertices = calculate_vertices(instructions)
+    return perimeter(instructions) // 2 + 1 + shoelace_formula(vertices)
 
-print(sorted(rows.items(), key=lambda x: x[0])[:2])
+party_1 = calculate_area(instructions_part_1)
+party_2 = calculate_area(instructions_part_2)
 
-party_1 = expand_inwards(outer_edge, start=-2-208j)
+print_solutions(party_1, party_2)
 
-print_solutions(party_1)
 
 def test_one():
     assert party_1 == 53300
 
+def test_two():
+    assert party_2 == 64294334780659

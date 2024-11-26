055677f1d02eace6b618f8d0dc47c9e89110b408
Maja Gacesa <maja.gacesa@gmail.com>
2024-11-26 21:36:12 +0100

    [day22] the trick is to keep a set of all to-be-removed slabs
    I did not come up with this by myself, it is almost copy-pasted from
    here:
    https://advent-of-code.xavd.id/writeups/2023/day/22/

--- python/22.py
+++ python/22.py
@@ -1,5 +1,5 @@
 from santas_little_helpers.helpers import *
-from collections import defaultdict, namedtuple
+from collections import defaultdict, namedtuple, deque
 from dataclasses import dataclass
 from itertools import product
 
@@ -26,7 +26,7 @@ class Slab:
                 self.supported_by.add(other.id)
     
     def __eq__(self, other) -> bool:
-        return self.installed_z == other.installed_z and self.projection == other.projection
+        return self.id == other.id
 
 
 
@@ -49,9 +49,7 @@ def parse_slabs(input_data):
 def find_supports(consolidated):
     for below, above in product(consolidated, repeat=2):
         above.calculate_supports(below)
-
-
-
+    return {slab.id: slab.supported_by for slab in consolidated}
 
 
 def consolidate(slabs):
@@ -71,25 +69,38 @@ def how_many_can_go_out(slabs):
     single_slab_supports = set()
     for slab in slabs:
         if len(slab.supported_by) == 1:
-            single_slab_supports.add(slab.supported_by.pop())
-    return len(consolidated) - len(single_slab_supports)
+            single_slab_supports.add(list(slab.supported_by)[0])
+    return len(consolidated) - len(single_slab_supports), single_slab_supports
+
+def generate_all_paths(supported_by, singles):
+    count = 0
+    for start in singles:
+        removed = {start}
+        while True:
+            would_fall = {slab for slab, supports in supported_by.items() if supports and supports <= removed}
+            if len(would_fall) == len(removed)-1:
+                count += len(would_fall)
+                break
+            removed |= would_fall
+    return count
+
 
 input_data = read_input(22)
 slabs = parse_slabs(input_data)
 
 conso = consolidate(slabs)
+slabs_supported_by = find_supports(conso)
 
 
 
-
-find_supports(conso)
-
-
-party_1 = how_many_can_go_out(slabs)
+party_1, singles = how_many_can_go_out(slabs)
+party_2 = generate_all_paths(slabs_supported_by, singles)
 
 
-
-print_solutions(party_1)
+print_solutions(party_1, party_2)
 
 def test_one():
     assert party_1 == 517
+
+def test_two():
+    assert party_2 == 61276
